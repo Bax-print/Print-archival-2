61a3d203721ec36b12abdc7a323208ded511428e
Bax-print <Bax@printarchival.ca>
2026-06-16 09:33:43 -0600

    Add theme setup include

---
+++ inc/setup.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Theme setup and widget registration.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Theme setup.
+ */
+function print_archival_setup() {
+
+	// Let WordPress manage the document title.
+	add_theme_support( 'title-tag' );
+
+	// Enable featured images.
+	add_theme_support( 'post-thumbnails' );
+
+	// Enable responsive embeds.
+	add_theme_support( 'responsive-embeds' );
+
+	// Enable HTML5 markup support.
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	// Enable custom logo support.
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 120,
+			'width'       => 400,
+			'flex-height' => true,
+			'flex-width'  => true,
+		)
+	);
+
+	// Register navigation menus.
+	register_nav_menus(
+		array(
+			'primary' => __( 'Primary Menu', 'print-archival' ),
+			'footer'  => __( 'Footer Menu', 'print-archival' ),
+		)
+	);
+}
+add_action( 'after_setup_theme', 'print_archival_setup' );
+
+/**
+ * Register widget areas.
+ */
+function print_archival_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer Widgets', 'print-archival' ),
+			'id'            => 'footer-widgets',
+			'description'   => __( 'Widgets shown in the footer.', 'print-archival' ),
+			'before_widget' => '<section id="%1$s" class="footer-widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="footer-widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'print_archival_widgets_init' );
