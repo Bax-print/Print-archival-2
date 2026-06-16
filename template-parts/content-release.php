911ccd1c88ddb4207451b893134e50474badd870
Bax-print <Bax@printarchival.ca>
2026-06-16 09:35:02 -0600

    Add release content template part

---
+++ template-parts/content-release.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Featured release card template part.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<article class="artwork-card">
+	<a href="<?php the_permalink(); ?>">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
+		<div class="artwork-card__content">
+			<p class="eyebrow">Featured Release</p>
+			<h3><?php the_title(); ?></h3>
+
+			<?php if ( has_excerpt() ) : ?>
+				<p><?php echo esc_html( get_the_excerpt() ); ?></p>
+			<?php endif; ?>
+		</div>
+	</a>
+</article>
