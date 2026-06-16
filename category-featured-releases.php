ae6790df471a1f7a7c40f676c06971b07ef76e4b
Bax-print <Bax@printarchival.ca>
2026-06-16 09:35:37 -0600

    Add plural featured releases category compatibility template

---
+++ category-featured-releases.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Compatibility wrapper for plural Featured Releases category template filename.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require get_template_directory() . '/category-featured-release.php';
