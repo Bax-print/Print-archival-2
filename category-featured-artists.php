d7ded931d2b2eff78c363a2777e1dd05ac3d0fa7
Bax-print <Bax@printarchival.ca>
2026-06-16 09:35:34 -0600

    Add plural featured artists category compatibility template

---
+++ category-featured-artists.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Compatibility wrapper for plural Featured Artists category template filename.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require get_template_directory() . '/category-featured-artist.php';
